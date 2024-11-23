0d9c51f106827ac861e0d26e30991cb1533dca3e
Steven <stevenaknobel@gmail.com>
2024-11-23 11:54:34 +0200

    added in 3 properties, including an array with default setup and a loop to present the properties

--- index.ts
+++ index.ts
@@ -5,9 +5,10 @@
 const reviewTotalDisplay = document.querySelector('#reviews')
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
+const propertyContainer = document.querySelector('.properties')
 
 let isOpen : boolean
-
+//reviews array
 const reviews: {
     name: string;
     stars: number;
@@ -33,14 +34,14 @@ const reviews: {
         date: '27-03-2021'
     },
 ]
-
+//review function
 function showReviewTotal (value : number, reviewer: string, isLoyalty: boolean) {
     const iconDisplay = isLoyalty ? "⭐" : ""
     reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last review by ' + reviewer + " " + iconDisplay
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
-
+//user info
 const you: {
     firstName : string;
     lastName: string;
@@ -54,8 +55,62 @@ const you: {
     age: 35,
     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
+//array of properties with info
+const properties: {
+    image: string;
+    title: string;
+    price: number;
+    location: {
+        firstLine: string;
+        city: string;
+        code: number;
+        country: string;
+    };
+    contact:string;
+    isAvailable: boolean;
+}[] = [
+    {
+        image: 'images/colombia-property.jpg',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: 'marywinkle@gmail.com',
+        isAvailable: true  
+    },
+    {
+        image: 'images/poland-property.jpg',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 343903,
+            country: 'Poland'
+        },
+        contact: 'garydavis@hotmail.com',
+        isAvailable: false 
+    },
+    {
+        image: 'images/london-property.jpg',
+        title: 'London Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 35433,
+            country: 'United Kingdom',
+        },
+        contact: 'andyluger@aol.com',
+        isAvailable: true
+    }
+]
 
-
+//functions
 function populateUser(isReturning : boolean, userName : string ) {
     if (isReturning){
         returningUserDisplay.innerHTML = 'back'
@@ -64,3 +119,14 @@ function populateUser(isReturning : boolean, userName : string ) {
 }
 
 populateUser(you.isReturning, you.firstName)
+
+//add the properties using a loop
+for (let i = 0; i < properties.length; i++) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML = properties[i].title
+    const image = document.createElement('img')
+    image.setAttribute('src', properties[i].image)
+    card.appendChild(image)
+    propertyContainer.appendChild(card)
+}
